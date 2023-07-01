4cf335a6c081ce845e26f4bfd0c0b0a64ee118d4
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-07-01 12:29:56 +0700

    update model oauth

--- src/main/java/com/investree/demo/model/Users.java
+++ src/main/java/com/investree/demo/model/Users.java
@@ -2,15 +2,20 @@ package com.investree.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
-public class Users implements Serializable {
+public class Users implements UserDetails, Serializable {
     public Users() {}
 
     public Users(Long id) {
@@ -31,4 +36,101 @@ public class Users implements Serializable {
 
     @Column(name = "is_active", nullable = false, columnDefinition = "BOOLEAN")
     private boolean is_active;
+
+    @JsonIgnore
+    private String verifyToken;
+
+    @JsonIgnore
+    private Date expiredVerifyToken;
+
+    @Column(length = 100, nullable = true)
+    private String otp;
+
+    private Date otpExpiredDate;
+
+    @JsonIgnore
+    private boolean enabled = true;
+
+    @JsonIgnore
+    @Column(name = "not_expired")
+    private boolean accountNonExpired = true;
+
+    @JsonIgnore
+    @Column(name = "not_locked")
+    private boolean accountNonLocked = true;
+
+    @JsonIgnore
+    @Column(name = "credential_not_expired")
+    private boolean credentialsNonExpired = true;
+
+    @ManyToMany(targetEntity = Role.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = {
+                    @JoinColumn(name = "user_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "role_id")
+            }
+    )
+    private List<Role> roles = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
